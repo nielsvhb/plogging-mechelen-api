f2ac8faa5835e53fc81b303f6d20706bd181b33d
Niels Vanhorenbeeck <nielsvhb@hotmail.com>
2020-12-01 22:45:30 +0100

    Added homepage

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite3',
+        client: 'sqlite',
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
