8d98204d18abbed0ba34e3252597fe54d9375829
Niels Vanhorenbeeck <nielsvhb@hotmail.com>
2020-12-01 22:14:28 +0100

    Database config changed to use node env

--- config/database.js
+++ config/database.js
@@ -5,11 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: 'ec2-54-228-250-82.eu-west-1.compute.amazonaws.com',
-        port: '5432',
-        database: 'd1bgsflnomh0dr',
-        username:'bwtwoevyzqvoha',
-        password: '5deec0f3e16032d278a6d6965cc1802942db707b0558cfa7398dccb9fbb5c53c',
+        host: `${process.env.DATABASE_HOST}`,
+        port: `${process.env.DATABASE_PORT}`,
+        database: `${process.env.DATABASE_NAME}`,
+        username: `${process.env.DATABASE_USERNAME}`,
+        password: `${process.env.DATABASE_PASSWORD}`,
+        ssl: { "rejectUnauthorized": false },
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
